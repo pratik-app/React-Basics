97e8ac60b10785eb62a79adbfb96e1284d7eb3d4
Pratik More <developer@pratikmore.com>
2023-06-06 17:51:39 -0400

    Understanding Event Handler
    Adding Click event that will print message as well as generate alert message that the button is pressed!

--- react-smile/src/components/learning.js
+++ react-smile/src/components/learning.js
@@ -10,9 +10,14 @@ class learning extends Component{
             {name:'apple',weight:'110gm'},
         ]
     }}
+    clickHandler = ()=>{
+        console.log('Click Me is pressed')
+        alert("Click Me is Pressed!")
+    }
     render(){
         return( 
         <div>
+            <button onClick= {this.clickHandler}>Click Me</button>
         <h1> Fruit used is {this.state.fruits[0].name}</h1>
         <h1> Fruit used is {this.state.fruits[1].name}</h1>
         <h1> Fruit used is {this.state.fruits[2].name}</h1>
