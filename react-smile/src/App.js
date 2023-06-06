c12bcaaee5fd6211191ebcbead8b01c3df26e5fc
Pratik More <developer@pratikmore.com>
2023-06-06 16:44:45 -0400

    Understanding Class Component
    This is Class Component.

--- react-smile/src/App.js
+++ react-smile/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/hello';
+import LComp from './components/learning';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Hello Pratik!</h1>
-
         <Hello/>
+
+        <LComp/>
       </header>
     </div>
   );
---
+++ react-smile/src/components/learning.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class learning extends Component{
+
+    render(){
+        return <h1> This is Class Component</h1>
+    }
+
+}
+
+export default learning
