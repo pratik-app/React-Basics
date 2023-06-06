fb6504b7751557e754235806da7aa6b737f502f4
Pratik More <developer@pratikmore.com>
2023-06-06 17:44:06 -0400

    Using Constructor
    Passing props in super to use states

--- react-smile/src/components/learning.js
+++ react-smile/src/components/learning.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 
 class learning extends Component{
-
-    state = {
+ constructor(props){
+    super(props)
+    this.state = {
         fruits:[
             {name:'mango',weight:'120gm'},
             {name:'grape',weight:'150gm'},
             {name:'apple',weight:'110gm'},
         ]
-    }
+    }}
     render(){
-        return <h1> Fruit used is {this.state.fruits[0].name}</h1>
+        return( 
+        <div>
+        <h1> Fruit used is {this.state.fruits[0].name}</h1>
+        <h1> Fruit used is {this.state.fruits[1].name}</h1>
+        <h1> Fruit used is {this.state.fruits[2].name}</h1>
+        </div>
+        )
     }
 
 }
