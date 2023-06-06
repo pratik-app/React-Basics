10d569d315c6a17619c1072870be3f60e8152601
Pratik More <developer@pratikmore.com>
2023-06-06 17:08:41 -0400

    Understanding States
    States are useful for storing data or information about the component.

--- react-smile/src/components/learning.js
+++ react-smile/src/components/learning.js
@@ -2,8 +2,15 @@ import React, { Component } from "react";
 
 class learning extends Component{
 
+    state = {
+        fruits:[
+            {name:'mango',weight:'120gm'},
+            {name:'grape',weight:'150gm'},
+            {name:'apple',weight:'110gm'},
+        ]
+    }
     render(){
-        return <h1> This is Class Component</h1>
+        return <h1> Fruit used is {this.state.fruits[0].name}</h1>
     }
 
 }
